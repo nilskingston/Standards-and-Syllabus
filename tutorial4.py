9cf02df3ddbfe4a506021adacfe1b67dea353a46
nilskingston <nilskingston@hanovernorwichschools.org>
2016-04-08 14:08:36 -0400

    Updated from Brython Server: 4/8/2016 2:08:38 PM

--- tutorial4.py
+++ tutorial4.py
@@ -3,7 +3,15 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-# Background
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
 
 class SpaceGame(App):
     """
@@ -15,6 +23,7 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
+
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
 
